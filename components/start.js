a010e1afb1f53e1e75c3cf3f02ba71763e02dbdf
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-03-20 16:56:36 -0400

    Creating styling for the input text

--- components/start.js
+++ components/start.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
 
 export default class Screen1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { text: '' }
+    }
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Hello Screen1!</Text>
+                <TextInput
+                    style={styles.input}>
+
+
+                </TextInput>
                 <Button
                     title="Go to Screen 2"
                     onPress={() => this.props.navigation.navigate('INSERT CHAT STUFF HERE')}
@@ -14,3 +22,15 @@ export default class Screen1 extends React.Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    input: {
+        fontSize: 16,
+        fontWeight: 300,
+        fontColor: '#757083',
+        opacity: 0.5,
+    }
+})
