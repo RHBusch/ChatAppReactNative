4325fc8182c7333eb3418bbb818434ef291e1d83
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-03-27 09:35:44 -0400

    Importing gifted chat and setting initial state + message functions

--- components/chat.js
+++ components/chat.js
@@ -1,21 +1,53 @@
 import React from 'react';
 import { View, Text, Button, TextInput } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+
 
 
 export default class ChatScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        }
+    }
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any'
+                    },
+                },
+            ],
+        })
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages)
+        }))
+    }
+
     render() {
         let name = this.props.route.params.name;
         this.props.navigation.setOptions({ title: name });
         const { bgColor } = this.props.route.params;
         return (
-            <View style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: bgColor
-            }}>
-                <Text> TESTING CHAT</Text>
-            </View>
+
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+
         )
     }
 }
